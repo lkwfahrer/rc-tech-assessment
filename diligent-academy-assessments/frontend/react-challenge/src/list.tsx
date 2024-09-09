482d59cf220603b0fcba476ea483d80b31772aa8
A.Shparev <shparevsasha@gmail.com>
2024-09-09 17:58:59 +0200

    Task 2. Own component

---
+++ diligent-academy-assessments/frontend/react-challenge/src/list.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect} from "react";
+import {callApi} from "./call-api";
+
+interface Hero {
+    id: number;
+    name: string;
+    available: boolean;
+}
+
+const List = () => {
+    const [items, setItems] = useState<Hero[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const getItems = async () => {
+            try {
+                const heroes: Hero[] = await callApi("heroes");
+                setItems(heroes)
+            }
+            catch (e) {
+                setError("Fetching failed");
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getItems();
+    }, []);
+
+    if (loading) return <p>Loading</p>;
+    if (error) return <p>{error}</p>;
+
+    return (
+        <ul style={{listStyleType: 'none', paddingLeft: 0}}>
+            {items.map((items: Hero) => (
+                <li key={items.id}>
+                    {items.id}. {items.name} {items.available ? items.available : null}
+                </li>
+            ))}
+
+        </ul>
+    );
+}
+
+export {List}
